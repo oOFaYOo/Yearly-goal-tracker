cd5c82e3551152b3357688f233cdb3ccf839b832
Nika Tseleva <nika.tseleva@mail.ru>
2023-02-11 15:00:59 +0400

    add search, filtering, sorting

--- src/components/SortingPanel/index.tsx
+++ src/components/SortingPanel/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import {FormControl, InputLabel, MenuItem, Select, Switch} from "@mui/material";
 
-const SortingPanel = ({years, setSorting}:{years:string[], setSorting:React.Dispatch<React.SetStateAction<number>>}) => {
+const SortingPanel = ({
+                          years,
+                          setSorting,
+                          setFiltering
+                      }: { years: string[], setSorting: React.Dispatch<React.SetStateAction<number>>,
+    setFiltering: React.Dispatch<React.SetStateAction<number>> }) => {
 
     return (
         <div className='flex items-center justify-around relative w-full gap-2 h-full'>
             <div className='flex flex-row items-center'><p>light</p>
-            <Switch color="default"
+                <Switch color="default"
                     // checked={}
                     // onChange={(e)=>{e.currentTarget.checked}}
-            />
+                />
                 <p>dark</p></div>
             <FormControl className='w-[150px]'>
                 <InputLabel id="demo-simple-select-label">Completed</InputLabel>
@@ -25,8 +30,8 @@ const SortingPanel = ({years, setSorting}:{years:string[], setSorting:React.Disp
                     }}
                 >
                     <MenuItem value={1}>not sorted</MenuItem>
-                    <MenuItem value={2}>1 → 100</MenuItem>
-                    <MenuItem value={3}>100 ← 1</MenuItem>
+                    <MenuItem value={2}>0 → 100</MenuItem>
+                    <MenuItem value={3}>100 ← 0</MenuItem>
                 </Select>
             </FormControl>
             <FormControl className='w-[150px]'>
@@ -37,14 +42,16 @@ const SortingPanel = ({years, setSorting}:{years:string[], setSorting:React.Disp
                     value={undefined}
                     label="Year"
                     className='min-w-[70px]'
-                    onChange={() => {
+                    onChange={(e) => {
+                        // @ts-ignore
+                        setFiltering(e.target.value)
                     }}
-                ><MenuItem value={years.length}>not filtered</MenuItem>
+                ><MenuItem value={1}>not filtered</MenuItem>
                     {
-                    years.map((v,i)=>{
-                        return <MenuItem key={i} value={i}>{v}</MenuItem>
-                    })
-                }
+                        years.map((v, i) => {
+                            return <MenuItem key={i} value={v}>{v}</MenuItem>
+                        })
+                    }
                 </Select>
             </FormControl>
         </div>
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -14,6 +14,8 @@ const Main = () => {
     const [openGoalEditingPanel, setOpenGoalEditingPanel] = useState<boolean>(false);
     const [data, setData] = useState<Goals|undefined>(undefined);
     const [sorting, setSorting] = useState<number>(1);
+    const [filtering, setFiltering] = useState<number>(1);
+    const [searchValue, setSearchValue] = useState('');
 
     useEffect(()=>{
         (async ()=>{
@@ -44,7 +46,7 @@ const Main = () => {
             </header>
             <div className='flex justify-between items-center py-4 h-24'>
                 <div className='flex px-6 items-center justify-center relative w-[40%] h-full'>
-                    <SortingPanel setSorting={setSorting} years={Object.keys(data)} />
+                    <SortingPanel setFiltering={setFiltering} setSorting={setSorting} years={Object.keys(data)} />
                 </div>
                 <div className='flex grow justify-center'>
                     <button onClick={()=>setOpenGoalCreationPanel(true)}
@@ -53,12 +55,20 @@ const Main = () => {
                     </button>
                 </div>
                 <div className='flex justify-center relative items-center w-[40%] h-full'>
-                    <input type={'text'} placeholder={'Search...'} className='outline-none w-[60%] h-[70%] rounded-full align-middle px-4 border-2 border-teal-500'/>
+                    <input type={'text'} placeholder={'Search...'}
+                           className='outline-none w-[60%] h-[70%] rounded-full align-middle px-4 border-2 border-teal-500'
+                    onChange={(e)=>{
+                        setSearchValue(e.currentTarget.value)
+                    }}
+                    />
                 </div>
             </div>
             {
                 data ? (
-                    Object.keys(data).sort((a,b)=>+b-(+a)).map((v, i) => {
+                    Object.keys(data).sort((a,b)=>+b-(+a)).filter((v)=>{
+                        if(filtering === 1) return v;
+                        if(+filtering === +v) return v;
+                    }).map((v, i) => {
                         return <>
                             <div key={i} className='my-2 flex flex-row justify-center items-center'>
                                 <div className='h-[1px] w-[40%] bg-gray-500'/>
@@ -69,19 +79,19 @@ const Main = () => {
                                 {
                                     (()=>{
                                         if(sorting === 1){
-                                            return data[v].map((v,i)=>{
+                                            return data[v].filter((v)=>v.name.toLowerCase().includes(searchValue.toLowerCase())).map((v,i)=>{
                                                 return <Tile key={i} name={v.name} steps={v.steps}
                                                              onClick={setOpenGoalEditingPanel}/>
                                             });
                                         } else if(sorting === 2){
-                                            return data[v].slice(0).sort((a, b)=>{
+                                            return data[v].filter((v)=>v.name.toLowerCase().includes(searchValue.toLowerCase())).slice(0).sort((a, b)=>{
                                                 return getPercent(a.steps) - getPercent(b.steps)
                                             }).map((v,i)=>{
                                                 return <Tile key={i} name={v.name} steps={v.steps}
                                                              onClick={setOpenGoalEditingPanel}/>
                                             })
                                         }else if(sorting === 3){
-                                            return data[v].slice(0).sort((a, b)=>{
+                                            return data[v].filter((v)=>v.name.toLowerCase().includes(searchValue.toLowerCase())).slice(0).sort((a, b)=>{
                                                 return getPercent(b.steps) - getPercent(a.steps)
                                             }).map((v,i)=>{
                                                 return <Tile key={i} name={v.name} steps={v.steps}
